70e3981326b55c986c4e1c10703fac31c90a5c42
OrlandoMendoza17 <ommv.17@gmail.com>
2021-05-09 12:12:33 -0200

    Desktop responsive del Hero

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -4,7 +4,10 @@ import mobileImage from '../../assets/images/image-mockups.png';
 const Hero = () => {
   return (
     <section className="Hero">
-      <div className="Hero__cover"></div>
+      <div className="Hero__cover">
+        {/* <img className="Hero__image" src={mobileImage} alt="" /> */}
+        <div className="Hero__image"></div>
+      </div>
       <div className="Hero__content">
         <h1 className="Hero__title">Next generation digital banking</h1>
         <p className="Hero__description">Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</p>
