5b18acc000bb3467f5ccfaf4be5c2f9e0fcde811
OrlandoMendoza17 <ommv.17@gmail.com>
2021-05-08 23:42:37 -0200

    Desktop responsive del Footer

--- src/components/widgets/Footer.jsx
+++ src/components/widgets/Footer.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import facebookIcon from '../../assets/icons/icon-facebook.svg';
-import youtubeIcon from '../../assets/icons/icon-youtube.svg';
-import twitterIcon from '../../assets/icons/icon-twitter.svg';
-import pinterestIcon from '../../assets/icons/icon-pinterest.svg';
-import instramIcon from '../../assets/icons/icon-instagram.svg';
+import Facebook from '../../components/icons/Facebook';
+import Youtube from '../../components/icons/Youtube';
+import Twitter from '../../components/icons/Twitter';
+import Pinterest from '../../components/icons/Pinterest';
+import Instagram from '../../components/icons/Instagram';
 import brandLogo from '../../assets/icons/logo-light.svg';
 
 const Footer = () => {
   return (
     <footer className="Footer">
-      <div className="Footer__logo">
-        <img className="Footer__logo-image" src={brandLogo} alt=""/>
+       
+      <div className="Footer__section Footer__section--start">
+        <img className="Footer__logo" src={brandLogo} alt=""/>
+        <div className="Footer__social-medias">
+          <a href="#?" className="Footer__social-media-links">
+            <Facebook className="Footer__icon"/>
+          </a>
+          <a href="#?" className="Footer__social-media-links">
+            <Youtube className="Footer__icon"/>
+          </a>
+          <a href="#?" className="Footer__social-media-links">
+            <Twitter className="Footer__icon"/>
+          </a>
+          <a href="#?" className="Footer__social-media-links">
+            <Pinterest className="Footer__icon"/>
+          </a>
+          <a href="#?" className="Footer__social-media-links">
+            <Instagram className="Footer__icon"/>
+          </a>
+        </div>
       </div>
       
-      <div className="Footer__social-medias">
-        <a href="#?" className="Footer__social-media-links">
-          <img src={facebookIcon} alt=""/>
-        </a>
-        <a href="#?" className="Footer__social-media-links">
-          <img src={youtubeIcon} alt=""/>
-        </a>
-        <a href="#?" className="Footer__social-media-links">
-          <img src={twitterIcon} alt=""/>
-        </a>
-        <a href="#?" className="Footer__social-media-links">
-          <img src={pinterestIcon} alt=""/>
-        </a>
-        <a href="#?" className="Footer__social-media-links">
-          <img src={instramIcon} alt=""/>
-        </a>
-      </div>
-      
-      <div className="Footer__links">
+      <div className="Footer__section Footer__links">
         <a className="Footer__link" href="#?">About Us</a>
         <a className="Footer__link" href="#?">Contact</a>
         <a className="Footer__link" href="#?">Blog</a>
@@ -40,13 +40,16 @@ const Footer = () => {
         <a className="Footer__link" href="#?">Privacy Policy</a>
       </div>
       
-      <button className="Footer__call-to-action">
-        Request Invite
-      </button>
-      
-      <div className="Footer__copyright">
-        <p>© Easybank. All Rights Reserved</p>
+      <div className="Footer__section Footer__section--end">
+        <button className="Footer__call-to-action">
+          Request Invite
+        </button>
+        
+        <div className="Footer__copyright">
+          <p>© Easybank. All Rights Reserved</p>
+        </div>
       </div>
+      
     </footer>
   )
 }
