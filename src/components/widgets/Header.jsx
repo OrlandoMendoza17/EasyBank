8398a81b821c2d8776e51e2eb52d7a8ee3e474fc
OrlandoMendoza17 <ommv.17@gmail.com>
2021-05-09 13:13:02 -0200

    Se creó la lógica de funcionamiento del navbar

--- src/components/widgets/Header.jsx
+++ src/components/widgets/Header.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import brandLogo from '../../assets/icons/logo.svg'
 import Hambuger from '../icons/Hamburger';
 
 const Header = () => {
+  const [navOpened, setNavOpened] = useState(false)
   return (
     <header className="Header">
       <div className="Header__logo">
         <img className="Header__logo-image" src={brandLogo} alt="" />
       </div>
-      <Hambuger className="Header__hamburger"/>
+      
+      <div className={`Header__overlay ${navOpened? 'Header__overlay--open' : ''}`}>
+        <div className="Header__links">
+          <a className="Header__link" href="#">Home</a>
+          <a className="Header__link" href="#">About</a>
+          <a className="Header__link" href="#">Contact</a>
+          <a className="Header__link" href="#">Blog</a>
+          <a className="Header__link" href="#">Careers</a>
+        </div>
+      </div>
+      
+      <button className="Header__call-to-action">
+        Request Invite
+      </button>
+      
+      <button 
+        className="Header__hamburger"
+        onClick={() => setNavOpened(!navOpened)}
+      >
+        <Hambuger className="Header__hamburger-icon"/>
+      </button>
     </header>
   )
 }
